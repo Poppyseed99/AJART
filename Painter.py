c8af8c2258b38bb877b114cda22e71e1998b6847
Poppyseed99 <xxmoonlightmanorxx@gmail.com>
2024-10-09 00:39:20 -0400

    Update Painter.py

--- Painter.py
+++ Painter.py
@@ -1,5 +1,4 @@
 import time
-
 import pyautogui
 import keyboard
 from PIL import Image
@@ -7,74 +6,41 @@ from math import floor
 
 
 def select_color(color):
-    pyautogui.click(53, 31)
     time.sleep(1)
     if color == "cyan":
-        pyautogui.click(198, 193)
+        pyautogui.click(121, 202)
     elif color == "magenta":
-        pyautogui.click(198, 286)
+        pyautogui.click(81, 299)
     elif color == "yellow":
-        pyautogui.click(198, 101)
-    elif color == "key" or color == "black":
-        pyautogui.click(131, 354)
+        pyautogui.click(123, 121)
     elif color == "red":
-        pyautogui.click(457, 165)
+        pyautogui.click(103, 82)
     elif color == "blue":
-        pyautogui.click(457, 409)
+        pyautogui.click(95, 230)
     elif color == "green":
-        pyautogui.click(457, 695)
+        pyautogui.click(102, 150)
     else:
         raise ValueError(f"Value {color} not in colors")
-    time.sleep(1)
-    pyautogui.click(1108, 60)
     time.sleep(1)
 
 
 def select_size(size):
-    pyautogui.click(53, 516)
     time.sleep(1)
     if size <= 3:
-        pyautogui.click(172, 866)
+        pyautogui.click(78, 786)
     elif size <= 7:
-        pyautogui.click(265, 866)
+        pyautogui.click(156, 784)
     elif size <= 13:
-        pyautogui.click(172, 917)
+        pyautogui.click(83, 834)
     elif size <= 20:
-        pyautogui.click(265, 917)
+        pyautogui.click(158, 828)
     elif size <= 40:
-        pyautogui.click(172, 980)
-    else:
-        pyautogui.click(265, 980)
+        pyautogui.click(76, 885)
     time.sleep(1)
-
-
-def gcr(im):
-    """
-    Basic "Gray Component Replacement" function. Returns a CMYK image with
-    percentage gray component removed from the CMY channels and put in the
-    K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41)
-    """
-    cmyk_im = im.split()
-    cmyk = []
-    for i in range(4):
-        cmyk.append(cmyk_im[i].load())
-    for x in range(im.size[0]):
-        for y in range(im.size[1]):
-            gray = int(
-                min(cmyk[0][x, y], cmyk[1][x, y], cmyk[2][x, y])
-            )
-            for i in range(3):
-                cmyk[i][x, y] = cmyk[i][x, y] - gray
-            cmyk[3][x, y] = gray
-    return Image.merge("CMYK", cmyk_im)
 
 
 def getcolors(mode):
-    if mode == "CMYK":
-        return ["cyan", "magenta", "yellow", "key"]
-    elif mode == "L":
-        return ["black"]
-    elif mode == "RGB":
+    if mode == "RGB":
         return ["red", "green", "blue"]
 
 
@@ -97,10 +63,8 @@ class Painter:
         self.scaledivisor = scaledivisor
         self.img = Image.open(filename)
         self.img = self.img.convert(self.mode)
-        if self.mode == "CMYK":
-            self.img = gcr(self.img)
-        self.width = 1380
-        self.height = 850
+        self.width = 703  # Canvas width for Animal Jam Classic
+        self.height = 430  # Canvas height for Animal Jam Classic
         self.img = self.img.resize((self.width // self.scaledivisor, self.height // self.scaledivisor))
         self.pix = self.img.load()
         self.x, self.y = self.img.size
@@ -109,7 +73,6 @@ class Painter:
         keyboard.add_hotkey("[", self.drawpic)
 
     def drawpic(self):
-        pyautogui.click(176, 699)
         time.sleep(1)
         select_size(self.scaledivisor)
 
@@ -137,6 +100,11 @@ class Painter:
                         print("unstopped")
                     xloc = 350 + xval * self.width / self.x
                     yloc = 110 + yval * self.height / self.y
+
+                    # Make sure the cursor stays within the canvas
+                    if xloc < 0 or xloc > self.width or yloc < 0 or yloc > self.height:
+                        continue
+
                     px = self.pix[xval, yval]
                     print(px)
                     if type(px) != int:
